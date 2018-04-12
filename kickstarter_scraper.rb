70cb95a2e285e12af5fe0c96872a6b219e2550c6
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2018-04-12 01:26:35 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -15,8 +15,9 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
 
   kickstarter = Nokogiri::HTML(html)
-  
-  binding.pry
+  projects = {}
+
+  # binding.pry
 end
 
 create_project_hash
